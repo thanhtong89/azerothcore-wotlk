892ed9bae3f821c46f36464f7062b7aeb892771d
Tong Pham <thanhtong89@gmail.com>
2019-07-18 22:27:25 -0700

    Adding playerbot from Single-AC project

---
+++ modules/bot/playerbot/strategy/AiObject.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "../PlayerbotAIAware.h"
+
+class PlayerbotAI;
+
+namespace BotAI
+{
+    class AiObjectContext;
+    class ChatHelper;
+
+    class AiObject : public PlayerbotAIAware
+	{
+	public:
+        AiObject(PlayerbotAI* ai);
+
+    protected:
+        Player* bot;
+        Player* GetMaster();
+        AiObjectContext* context;
+        ChatHelper* chat;
+	};
+
+    class AiNamedObject : public AiObject
+    {
+    public:
+        AiNamedObject(PlayerbotAI* ai, string name) : AiObject(ai), name(name) {}
+
+    public:
+        virtual string getName() { return name; }
+
+    protected:
+        string name;
+    };
+}
