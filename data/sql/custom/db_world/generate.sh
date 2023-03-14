454ad2e242e24235a8cccc374d71507a14a0caf4
Tong Pham <thanhtong89@gmail.com>
2023-03-14 01:25:58 -0700

    Addressing the removal of player_levelstats.sql, replaced by player_class_stats.sql

--- data/sql/custom/db_world/generate.sh
+++ data/sql/custom/db_world/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for fname in pet_levelstats player_levelstats player_classlevelstats; do
+for fname in pet_levelstats player_class_stats player_classlevelstats; do
 	echo "Generating ${fname}..."
 	./${fname}.sh > ${fname}.sql
 done
---
+++ data/sql/custom/db_world/player_class_stats.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Generate player_class_stats.sql custom SQL with level81-100 support
+
+mults="81:3-1.2,85:2-1,86:3-1.2,90:2-1,91:3-1.2,100:2-1"
+tmpfile=$(mktemp /tmp/player_class_stats.sql80.XXX)
+grep -Eo "\([0-9]+, 80, .*" ../../base/db_world/player_class_stats.sql > $tmpfile
+data=$(./generate_stats.py $tmpfile 1 "${mults}")
+
+template=$(cat << TT
+ALTER TABLE player_class_stats MODIFY COLUMN Strength INT unsigned;
+ALTER TABLE player_class_stats MODIFY COLUMN Agility INT unsigned;
+ALTER TABLE player_class_stats MODIFY COLUMN Stamina INT unsigned;
+ALTER TABLE player_class_stats MODIFY COLUMN Intellect INT unsigned;
+ALTER TABLE player_class_stats MODIFY COLUMN Spirit INT unsigned;
+
+REPLACE INTO player_class_stats (Class, Level, Strength, Agility, Stamina, Intellect, Spirit) VALUES
+${data}
+TT
+)
+
+echo "$template"
